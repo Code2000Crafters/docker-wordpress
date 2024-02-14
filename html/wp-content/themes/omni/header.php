ad33e359b0e71c79f70c01887efdab2bb444555d
nadzmihisaree <nanduni.20191258@iit.ac.lk>
2024-02-15 03:08:44 +0530

    navbar n' footer

--- html/wp-content/themes/omni/header.php
+++ html/wp-content/themes/omni/header.php
@@ -1,8 +1,3 @@
-<?php
-/* Development by : Nanduni */
-?>
-
-
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
 
@@ -15,17 +10,28 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <!-- Style sheets -->
+    <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/assets/css/styles.css">
+
+
 </head>
 
 <body <?php body_class(); ?>>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+
+            <a class="" href="#">
+                <img src="../assets/images/home_banner.png" alt="" style="height: 80px; width: 100px">
+
+            </a>
+
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -50,10 +56,29 @@
                         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                     </li>
                 </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+            </div> -->
+
+            <div class="d-flex align-items-center">
+                <div class="px-4">
+                    <a href="" class="nav-item fw-500 text-decoration-none" style="color: var(--black);">About</a>
+                </div>
+                <div class="px-4">
+                    <a href="" class="nav-item fw-500 text-decoration-none" style="color: var(--black);">Our Solutions</a>
+                </div>
+                <div class="px-4">
+                    <a href="" class="nav-item fw-500 text-decoration-none" style="color: var(--black);">Insights</a>
+                </div>
+                <div class="px-4">
+                    <a href="" class="nav-item fw-500 text-decoration-none" style="color: var(--black);">Careers</a>
+                </div>
+
+                <div class="px-4">
+                    <button class="nav_button py-2 px-4">Contact Us</button>
+                </div>
+
             </div>
+
         </div>
     </nav>
+
+    <main class="bg-white">
--- html/wp-content/themes/omni/page-home.php
+++ html/wp-content/themes/omni/page-home.php
@@ -6,5 +6,29 @@
  * @author Nanduni | 
  */
 
-?>
-<?php get_header(); ?>
+get_header(); ?>
+
+<!-- Section 1-->
+<section>
+
+    <div class="d-flex flex-column justify-content-center" style="background-image: url(http://i54.tinypic.com/4zuxif.jpg) no-repeat center;
+    background-size: cover; height: 90vh;">
+        <div>
+            <div class="col-12 col-lg-6 mx-auto text-center">
+                <div class="">
+                    vd,mmv,NFBknB
+                </div>
+
+
+                <div class="" >
+                    knbnnnnnnnnnnnnnnnkldssss
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--/ Section 1-->
+
+<div class="">vkmxm.bfmb</div>
+
+<?php get_footer(); ?>
